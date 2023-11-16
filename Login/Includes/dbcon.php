cb2b0fbaccf597c143138f3bb2f29a63eee77dda
Evans <@github.com:E-vanson/Node_tutorial.git>
2023-11-17 02:36:10 +0300

    First commit

---
+++ Login/Includes/dbcon.php
@@ -0,0 +1,4 @@
+<?php
+$port=3307;
+$con = mysqli_connect("localhost","root","","User Login",$port);
+?>
---
+++ Login/Includes/login.php
@@ -0,0 +1,36 @@
+<?php
+$page_title="Registration page";
+ include('/home/e-vans/projects/Sign up/Login/Includes/header.php');
+ include('/home/e-vans/projects/Sign up/Login/Includes/navbar.php');
+ ?>
+
+<div class="py-5">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-5">
+                <div class="card shadow">
+                    <div class="card-header">
+                        <h5>Login form</h5>
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group mb-3">
+                                <label for="">Email</label>
+                                <input type="text" name="email" class="from-control">
+                            </div>
+                            <div class="form-group mb-3">
+                                <label for="">Password</label>
+                                <input type="text" name="password" class="from-control">
+                            </div>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<?php include('/home/e-vans/projects/Sign up/Login/Includes/footer.php');?> 

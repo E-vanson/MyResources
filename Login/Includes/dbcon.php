9eefcc50aab9de7673a1f201a823a81a32fab781
Evans <@github.com:E-vanson/Node_tutorial.git>
2023-11-18 22:05:28 +0300

    db connection success

--- Login/Includes/dbcon.php
+++ Login/Includes/dbcon.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-$port=3307;
-$con = mysqli_connect("localhost","root","","User Login",$port);
+$con = mysqli_connect("localhost:3307","root","","UserLogin");
+if(mysqli_connect_errno()){
+    echo "Couldn't connect....error" . mysqli_connect_errno();
+}
 ?>
+</body>
+</html>
+

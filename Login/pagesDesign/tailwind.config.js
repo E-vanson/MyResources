6b067bfea7e0fd26ffb26b810214ea25431cdfa6
Victor Kang'acha <victorkangacha@gmail.com>
2023-11-21 03:54:24 +0300

    Style First Commit

---
+++ Login/pagesDesign/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screen: {
+      "lg": "1024px",
+      "sm": "496px"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+

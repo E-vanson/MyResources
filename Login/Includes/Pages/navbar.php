845e6fa1eda364caab4b3d15ef7de8adefca1a08
Evans <@github.com:E-vanson/Node_tutorial.git>
2023-11-21 14:03:46 +0300

    authenticate to log in

--- Login/Includes/Pages/navbar.php
+++ Login/Includes/Pages/navbar.php
@@ -13,15 +13,23 @@
         <li class="nav-item">
           <a class="nav-link active"  href="index.php">Home</a>
         </li>
+        <?php if(!isset($_SESSION['authenticated'])) : ?>
         <li class="nav-item">
           <a class="nav-link" href="register.php">Register</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="login.php">Login</a>
         </li>
+        <?php endif?>
         <li class="nav-item">
           <a class="nav-link" href="dashboard.php">Dashboard</a>
         </li>
+
+        <?php if(isset($_SESSION['authenticated'])) : ?>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">Logout</a>
+        </li>
+        <?php endif?>
       </ul>
     </div>
   </div>
